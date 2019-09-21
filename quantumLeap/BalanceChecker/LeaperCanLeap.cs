c6e7a4f254819e275b61b9b8382a338fda95b0ae
Silvestre Luna <slvtrluna@gmail.com>
2019-09-21 09:57:13 -0500

    Added a leapcharge

---
+++ quantumLeap/BalanceChecker/LeaperCanLeap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quantumLeap.BalanceChecker
+{
+    class LeaperCanLeap
+    {
+        public void LeapCharge()
+        {
+            Console.WriteLine("is working");
+        }
+    }
+}
